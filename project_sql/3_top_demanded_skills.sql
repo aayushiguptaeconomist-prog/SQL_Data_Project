2a995570c2fff6d1f44e03cea6ac3a02b711f871
Aayushi Gupta <aayushiguptaeconomist@gmail.com>
2025-11-13 18:36:48 -0700

    sql file modified

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -13,13 +13,11 @@ SELECT
 FROM
     job_postings_fact
 INNER JOIN skills_job_dim on job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim on skills_job_dim.job_id = skills_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst' AND
     job_work_from_home = TRUE
 GROUP BY
     skills
 ORDER BY
-    demand_count
-LIMIT
-    5;
+    demand_count DESC;
